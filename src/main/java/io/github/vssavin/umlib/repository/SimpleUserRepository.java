d2d6f8ff7fdba370c6b7d06c0d11f9fff71bbf35
svs-devel <example@example.com>
2023-06-17 10:59:16 +0300

    SimpleUserRepository: findByLogin, findAll(pageable), findAll(predicate, pageable) methods implemented

--- src/main/java/io/github/vssavin/umlib/repository/SimpleUserRepository.java
+++ src/main/java/io/github/vssavin/umlib/repository/SimpleUserRepository.java
@@ -1,42 +1,44 @@
 package io.github.vssavin.umlib.repository;
 
-import com.querydsl.core.types.ConstructorExpression;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.Predicate;
-import com.querydsl.core.types.QBean;
-import com.querydsl.sql.Configuration;
-import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.core.types.*;
+import com.querydsl.sql.*;
+import com.querydsl.sql.postgresql.PostgreSQLQueryFactory;
 import io.github.vssavin.umlib.config.DataSourceSwitcher;
 import io.github.vssavin.umlib.entity.QUser;
 import io.github.vssavin.umlib.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.inject.Provider;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
 import static com.querydsl.core.types.Projections.bean;
-import static com.querydsl.core.types.Projections.constructor;
 
 /**
  * @author vssavin on 16.06.2023
  */
 @Repository
-public class SimpleUserRepository implements UserRepository{
+public class SimpleUserRepository implements UserRepository {
+
+    private static final QUser users = new QUser("users");
+    private static final QBean<User> userBean = bean(User.class, users.id, users.login, users.name, users.password, users.email,
+            users.authority, users.expiration_date, users.verification_id);
 
     private final DataSourceSwitcher dataSourceSwitcher;
     private final Configuration queryDslConfiguration;
-    private final QUser users = new QUser("users");
 
-    private final QBean<User> userBean = bean(User.class, users.id, users.login, users.name, users.password, users.email,
-            users.authority, users.expiration_date, users.verification_id);
+    private AbstractSQLQueryFactory<?> queryFactory;
 
     @Autowired
     public SimpleUserRepository(DataSourceSwitcher dataSourceSwitcher, Configuration queryDslConfiguration) {
@@ -46,20 +48,17 @@ public class SimpleUserRepository implements UserRepository{
 
     @Override
     public List<User> findByLogin(String login) {
-        DataSource dataSource = dataSourceSwitcher.getCurrentDataSource();
-        SQLQueryFactory queryFactory =
-                new SQLQueryFactory(queryDslConfiguration, dataSource);
-        return queryFactory.select(userBean).from(users).where(users.login.eq(login)).fetch();
+        return prepareQuery(false).where(users.login.eq(login)).fetch();
     }
 
     @Override
     public List<User> findUserByName(String name) {
-        return null;//TODO: implement this
+        return new ArrayList<>();//TODO: implement this
     }
 
     @Override
     public List<User> findByEmail(String email) {
-        return null;//TODO: implement this
+        return new ArrayList<>();//TODO: implement this
     }
 
     @Override
@@ -74,7 +73,7 @@ public class SimpleUserRepository implements UserRepository{
 
     @Override
     public Iterable<User> findAll(Predicate predicate) {
-        return null;//TODO: implement this
+        return new ArrayList<>();//TODO: implement this
     }
 
     @Override
@@ -94,12 +93,17 @@ public class SimpleUserRepository implements UserRepository{
 
     @Override
     public Page<User> findAll(Pageable pageable) {
-        return null;//TODO: implement this
+        long totalCount = prepareQuery(false).fetchCount();
+        return new PageImpl<>(prepareQuery(true)
+                .limit(pageable.getPageSize()).offset(pageable.getOffset()).fetch(), pageable,
+                totalCount);
     }
 
     @Override
     public Page<User> findAll(Predicate predicate, Pageable pageable) {
-        return null;//TODO: implement this
+        long totalCount = prepareQuery(false).where(predicate).fetchCount();
+        return new PageImpl<>(prepareQuery(true).where(predicate)
+                .limit(pageable.getPageSize()).offset(pageable.getOffset()).fetch(), pageable, totalCount);
     }
 
     @Override
@@ -111,6 +115,7 @@ public class SimpleUserRepository implements UserRepository{
     public boolean exists(Predicate predicate) {
         throw new UnsupportedOperationException("Not implemented yet!");
     }
+
 
     @Override
     public <S extends User, R> R findBy(Predicate predicate,
@@ -125,8 +130,8 @@ public class SimpleUserRepository implements UserRepository{
 
     @Override
     public <S extends User> S save(S entity) {
-        return null; //TODO: implement this
-        //throw new UnsupportedOperationException("Not implemented yet!");
+        //Check if entity exists or not exists
+        return entity;  //TODO: implement this
     }
 
     @Override
@@ -146,12 +151,12 @@ public class SimpleUserRepository implements UserRepository{
 
     @Override
     public Iterable<User> findAll() {
-        return null;//TODO: implement this
+        return new ArrayList<>();//TODO: implement this
     }
 
     @Override
     public Iterable<User> findAllById(Iterable<Long> ids) {
-        return null;//TODO: implement this
+        return new ArrayList<>();//TODO: implement this
     }
 
     @Override
@@ -182,5 +187,36 @@ public class SimpleUserRepository implements UserRepository{
     @Override
     public void deleteAll() {
         throw new UnsupportedOperationException("Not implemented yet!");
+    }
+
+    private AbstractSQLQuery<User,?> prepareQuery(boolean useLastQueryFactory) {
+        if (useLastQueryFactory && queryFactory != null) {
+            return queryFactory.select(userBean).from(users);
+        }
+        DataSource dataSource = dataSourceSwitcher.getCurrentDataSource();
+        if (queryDslConfiguration.getTemplates() instanceof PostgreSQLTemplates) {
+            queryFactory = new PostgreSQLQueryFactory(queryDslConfiguration, new DataSourceProvider(dataSource));
+        }
+        else queryFactory = new SQLQueryFactory(queryDslConfiguration, dataSource);
+        return queryFactory.select(userBean).from(users);
+    }
+
+    private static class DataSourceProvider implements Provider<Connection> {
+
+        private final DataSource ds;
+
+        public DataSourceProvider(DataSource ds) {
+            this.ds = ds;
+        }
+
+        @Override
+        public Connection get() {
+            try {
+                return ds.getConnection();
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+
     }
 }
