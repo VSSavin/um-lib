0d7b0b55d4f6a7413622c93cff18621e12782f5d
vssavin <savin1801@gmail.com>
2023-06-22 16:13:53 +0300

    DefaultSecurityConfig: code migration to spring-security 5.8.0 (5.7.0 is still supported)

--- src/main/java/io/github/vssavin/umlib/config/DefaultSecurityConfig.java
+++ src/main/java/io/github/vssavin/umlib/config/DefaultSecurityConfig.java
@@ -5,19 +5,22 @@ import io.github.vssavin.umlib.service.impl.CustomOAuth2UserService;
 import io.github.vssavin.umlib.utils.AuthorizedUrlPermission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -25,7 +28,7 @@ import java.util.List;
 /**
  * Created by vssavin on 17.05.2022.
  */
-public class DefaultSecurityConfig extends WebSecurityConfigurerAdapter {
+public class DefaultSecurityConfig {
 
     public static final String LOGIN_URL = UmConfig.LOGIN_URL;
     public static final String LOGIN_PROCESSING_URL = UmConfig.LOGIN_PROCESSING_URL;
@@ -65,30 +68,24 @@ public class DefaultSecurityConfig extends WebSecurityConfigurerAdapter {
         umConfig.updateAuthorizedPermissions();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, UserDetailsService userDetailsService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder)
-                .usersByUsernameQuery(
-                        "select login, password, 'true' from users " +
-                                "where login=?")
-                .authoritiesByUsernameQuery(
-                        "select login, authority from users " +
-                                "where login=?");
+                .and()
+                .authenticationProvider(authProvider)
+                .build();
     }
 
     @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsManager() {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
-        jdbcUserDetailsManager.setDataSource(dataSource);
-        return jdbcUserDetailsManager;
+    public UserDetailsService userDetailsService() {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.addFilterBefore(
                 new BannedIpFilter(), BasicAuthenticationFilter.class);
 
@@ -97,11 +94,10 @@ public class DefaultSecurityConfig extends WebSecurityConfigurerAdapter {
 
         List<AuthorizedUrlPermission> urlPermissions = UmConfig.getAuthorizedUrlPermissions();
 
-        urlPermissions.add(new AuthorizedUrlPermission("/games/**", new String[]{"ADMIN", "USER"}));
         urlPermissions.add(new AuthorizedUrlPermission("/oauth/**", new String[]{}));
         urlPermissions.add(new AuthorizedUrlPermission("/login/oauth2/code/google", new String[]{}));
 
-        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry =
+        AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry =
                 registerUrls(http, urlPermissions);
 
         registry.and()
@@ -128,6 +124,8 @@ public class DefaultSecurityConfig extends WebSecurityConfigurerAdapter {
                     .userInfoEndpoint()
                     .userService(customOAuth2UserService);
         }
+
+        return http.build();
     }
 
     public static void setSuccessUrl(String successUrl) {
@@ -138,16 +136,16 @@ public class DefaultSecurityConfig extends WebSecurityConfigurerAdapter {
         DefaultSecurityConfig.adminSuccessUrl = adminSuccessUrl;
     }
 
-    private ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registerUrls(
+    private AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registerUrls(
             HttpSecurity http, List<AuthorizedUrlPermission> urlPermissions) throws Exception {
 
-        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry =
-                http.authorizeRequests();
+        AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry =
+                http.authorizeHttpRequests();
 
         for(AuthorizedUrlPermission urlPermission : urlPermissions) {
             String[] roles = urlPermission.getRoles();
-            ExpressionUrlAuthorizationConfigurer<HttpSecurity>.AuthorizedUrl authorizedUrl =
-                    registry.antMatchers(urlPermission.getUrl());
+            AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizedUrl authorizedUrl =
+                    registry.requestMatchers(new AntPathRequestMatcher(urlPermission.getUrl(), null));
 
             if (roles != null && roles.length == 0) {
                 registry = authorizedUrl.permitAll();
