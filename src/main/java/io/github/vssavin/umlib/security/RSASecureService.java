23c4bd4579f46355224ce391c900fd8520ce0dad
vssavin <savin1801@gmail.com>
2023-06-29 10:44:27 +0300

    RSASecureService: changed annotation to @Service

--- src/main/java/io/github/vssavin/umlib/security/RSASecureService.java
+++ src/main/java/io/github/vssavin/umlib/security/RSASecureService.java
@@ -3,11 +3,12 @@ package io.github.vssavin.umlib.security;
 import io.github.vssavin.securelib.RSASecure;
 import io.github.vssavin.umlib.security.SecureService;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * @author vssavin on 24.01.22
  */
-@Component
+@Service
 public class RSASecureService extends RSASecure implements SecureService {
     @Override
     public String toString() {
