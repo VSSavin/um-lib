6164c06e964d3eb984415be160f77c1ee20ae248
vssavin <savin1801@gmail.com>
2023-08-23 12:40:11 +0300

    UserGeneratorService improvements: improved count users initialization; the class moved to user package

--- src/test/java/com/github/vssavin/umlib/user/UserGeneratorService.java
+++ src/test/java/com/github/vssavin/umlib/user/UserGeneratorService.java
@@ -1,8 +1,5 @@
-package com.github.vssavin.umlib.email;
+package com.github.vssavin.umlib.user;
 
-import com.github.vssavin.umlib.user.UserExistsException;
-import com.github.vssavin.umlib.user.UserService;
-import com.github.vssavin.umlib.user.Role;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.DependsOn;
@@ -17,29 +14,31 @@ import javax.annotation.PostConstruct;
 @Service
 @DependsOn("sqlScriptsConfig")
 class UserGeneratorService {
+    private static final int DEFAULT_USERS_COUNT = 10;
     private final Logger log = LoggerFactory.getLogger(UserGeneratorService.class);
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final int countUsers;
 
     public UserGeneratorService(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
-    }
-
-    @PostConstruct
-    public void generateUsers() {
         String countUsersString = System.getProperty("userGenerator.count");
-        int count = 10;
-
+        int tmpCountUsers = DEFAULT_USERS_COUNT;
         if (countUsersString != null) {
             try {
-                count = Integer.parseInt(countUsersString);
+                tmpCountUsers = Integer.parseInt(countUsersString);
             } catch (NumberFormatException e) {
                 log.error("'userGenerator.count' property should be integer number");
             }
         }
 
-        for(int i = 0; i < count; i++) {
+        countUsers = tmpCountUsers;
+    }
+
+    @PostConstruct
+    public void generateUsers() {
+        for(int i = 0; i < countUsers; i++) {
             String login = String.valueOf(i);
             String name = String.valueOf(i);
             String password = passwordEncoder.encode(login);
@@ -52,5 +51,9 @@ class UserGeneratorService {
             }
 
         }
+    }
+
+    public UserService getUserService() {
+        return userService;
     }
 }
