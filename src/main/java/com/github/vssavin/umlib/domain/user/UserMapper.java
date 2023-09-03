061e3245cad9ffca8ca7e7b40f12aee377404906
svs-devel <example@example.com>
2023-09-03 14:19:51 +0300

    UserMapper: javadoc description fix

--- src/main/java/com/github/vssavin/umlib/domain/user/UserMapper.java
+++ src/main/java/com/github/vssavin/umlib/domain/user/UserMapper.java
@@ -3,6 +3,8 @@ package com.github.vssavin.umlib.domain.user;
 import org.mapstruct.Mapper;
 
 /**
+ * Mapper to convert a user entity to a corresponding data transfer object and vice versa.
+ *
  * @author vssavin on 29.08.2023
  */
 @Mapper(componentModel = "spring")
