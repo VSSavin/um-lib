94aba3f5038f1e36319fc2fa7d90f348acd6d50d
vssavin <savin1801@gmail.com>
2023-11-08 17:21:56 +0300

    RefreshOnLoginDatabaseTokenBasedRememberMeServiceTest: refactoring - extend AbstractRememberMeServiceTest; removed unnecessary tests

--- src/test/java/com/github/vssavin/umlib/domain/sucurity/rememberme/RefreshOnLoginDatabaseTokenBasedRememberMeServiceTest.java
+++ src/test/java/com/github/vssavin/umlib/domain/sucurity/rememberme/RefreshOnLoginDatabaseTokenBasedRememberMeServiceTest.java
@@ -1,23 +1,13 @@
 package com.github.vssavin.umlib.domain.sucurity.rememberme;
 
-import com.github.vssavin.umlib.domain.security.rememberme.Authenticator;
 import com.github.vssavin.umlib.domain.security.rememberme.RefreshOnLoginDatabaseTokenBasedRememberMeService;
-import com.github.vssavin.umlib.domain.security.rememberme.UserRememberMeTokenRepository;
-import com.github.vssavin.umlib.domain.user.User;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -26,74 +16,44 @@ import static org.mockito.Mockito.*;
 /**
  * @author vssavin on 07.11.2023
  */
-@RunWith(MockitoJUnitRunner.class)
-public class RefreshOnLoginDatabaseTokenBasedRememberMeServiceTest {
+public class RefreshOnLoginDatabaseTokenBasedRememberMeServiceTest extends AbstractRememberMeServiceTest {
 
-    private final String rememberMeCookieName = "remember-me";
-
-    private final long adminUserId = 1;
-
-    private final User adminUser = new User("admin", "admin", "admin", "admin@example.com", "ROLE_ADMIN");
-
-    private final String wrongRememberMeCookieValue = "abcd";
-
-    @Mock
-    private UserDetailsService userDetailsService;
-
-    @Mock
-    private UserRememberMeTokenRepository userRememberMeTokenRepository;
-
-    private RefreshOnLoginDatabaseTokenBasedRememberMeService rememberMeServices;
-
-    private Authenticator authenticator;
-
-    @Before
-    public void setUp() {
-        adminUser.setId(adminUserId);
+    @Override
+    protected void init() {
         rememberMeServices = new RefreshOnLoginDatabaseTokenBasedRememberMeService(UUID.randomUUID().toString(),
                 userDetailsService, userRememberMeTokenRepository);
-        authenticator = rememberMeServices;
         rememberMeServices.setCookieName(rememberMeCookieName);
 
         when(userDetailsService.loadUserByUsername(adminUser.getLogin())).thenReturn(adminUser);
     }
 
     @Test
-    public void shouldAuthenticatorReturnNullWhenNoCookieDetected() {
-        HttpServletRequest request = new MockHttpServletRequest();
-        Authentication authentication = authenticator.retrieveAuthentication(request, new MockHttpServletResponse());
-        Assert.assertNull(authentication);
-    }
-
-    @Test
-    public void shouldAuthenticatorReturnNullWhenWrongCookieDetected() {
+    public void shouldTokenRepositorySaveTokenWhenLoginSuccess() {
         MockHttpServletRequest request = new MockHttpServletRequest();
-        Cookie rememberMeCookie = new Cookie(rememberMeCookieName, wrongRememberMeCookieValue);
-        request.setCookies(rememberMeCookie);
-
-        MockHttpServletResponse response = new MockHttpServletResponse();
-
-        Authentication authentication = authenticator.retrieveAuthentication(request, response);
-        Assert.assertNull(authentication);
-    }
-
-    @Test
-    public void shouldAuthenticatorReturnAuthenticationWhenCookieOk() {
-        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addParameter(rememberMeServices.getParameter(), "true");
         MockHttpServletResponse response = new MockHttpServletResponse();
         Authentication authentication = new UsernamePasswordAuthenticationToken(adminUser, adminUser.getPassword());
-        rememberMeServices.onLoginSuccess(request, response, authentication);
+
+        rememberMeServices.loginSuccess(request, response, authentication);
 
         verify(userRememberMeTokenRepository, atLeastOnce()).save(any());
 
-        Cookie rememberMeCookie = response.getCookie(rememberMeCookieName);
+    }
 
+    @Test
+    public void shouldTokenRepositorySaveTokenWhenAutoLoginSuccess() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addParameter(rememberMeServices.getParameter(), "true");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(adminUser, adminUser.getPassword());
+
+        rememberMeServices.loginSuccess(request, response, authentication);
+        Cookie rememberMeCookie = response.getCookie(rememberMeCookieName);
         request.setCookies(rememberMeCookie);
 
-        authentication = authenticator.retrieveAuthentication(request, response);
-        Assert.assertNotNull("Authentication shouldn't be null for requested user!", authentication);
-        Assert.assertNotNull("Credentials shouldn't be null for requested user!", authentication.getCredentials());
-        Assert.assertNotNull("Principal shouldn't be null for requested user!", authentication.getPrincipal());
+        rememberMeServices.autoLogin(request, response);
+
+        verify(userRememberMeTokenRepository, atLeast(2)).save(any());
     }
 
 }
