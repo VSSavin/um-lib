400b865adc663f6498cbd8d09b59551e14e3cb3e
vssavin <savin1801@gmail.com>
2023-11-12 13:18:20 +0300

    UserRememberMeTokenRepository: changing the datasource using the @UmRouteDatasource annotation

--- src/main/java/com/github/vssavin/umlib/domain/security/rememberme/UserRememberMeTokenRepository.java
+++ src/main/java/com/github/vssavin/umlib/domain/security/rememberme/UserRememberMeTokenRepository.java
@@ -1,5 +1,6 @@
 package com.github.vssavin.umlib.domain.security.rememberme;
 
+import com.github.vssavin.umlib.config.aspect.UmRouteDatasource;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Repository
 public interface UserRememberMeTokenRepository extends CrudRepository<UserRememberMeToken, Long> {
 
+    @UmRouteDatasource
     List<UserRememberMeToken> findByUserId(Long userId);
 
 }
